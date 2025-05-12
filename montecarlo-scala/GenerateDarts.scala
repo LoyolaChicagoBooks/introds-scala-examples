fa8aef6f7fcddb60a417d37c6ce40005c318cd3f
Konstantin Läufer <laufer@cs.luc.edu>
2025-05-12 15:15:04 -0500

    added newlines for readability
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- montecarlo-scala/GenerateDarts.scala
+++ montecarlo-scala/GenerateDarts.scala
@@ -14,15 +14,18 @@ object GenerateDarts:
     @arg(short = 'n', doc = "Number of darts to generate") n: Int = 1000000,
     @arg(short = 'o', doc = "Output CSV file") output: String = "darts.csv"
   ): Unit =
+    
     val random = scala.util.Random
 
     Files.writeString(Path.of(output), s"x,y,inside\n", StandardCharsets.UTF_8)
+
     val sb = new StringBuilder
     (1 to n).foreach: i =>
       val x = random.nextDouble() * 2 - 1
       val y = random.nextDouble() * 2 - 1
       val inside = x * x + y * y <= 1
       sb.append(s"$x,$y,$inside\n")
+
     Files.writeString(Path.of(output), sb, StandardCharsets.UTF_8, StandardOpenOption.APPEND)
 
     println(s"Generated $n darts and saved to $output")
